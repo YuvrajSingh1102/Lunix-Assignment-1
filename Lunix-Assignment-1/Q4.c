dfd78d5603707f769b6c8f71219b798959a04563
Yuvraj Singh Solanki <98091421+YuvrajSingh1102@users.noreply.github.com>
2022-03-11 23:30:22 +0530

    Update Q4.c

--- Lunix-Assignment-1/Q4.c
+++ Lunix-Assignment-1/Q4.c
@@ -13,10 +13,13 @@ int main() {
     char read_buff[100];
 
     fd1 = creat("lunix2.txt", 777);         //Creating a new file
+    
     fd2 = open("lunix1.txt", O_RDONLY, 777);            //Opening a existing file in read only mode
 
     len = read(fd2, read_buff, 100);            //Reading the data from fd2 file and storing it in read_buff
+   
     //printf("Seeing data stored in len = %d\n\n", len);            //checking whether data is storing in read_buff or not
+   
     printf("Showing the data in stored in read_buff = \n%s\n\n", read_buff);
 
     len = write(fd1, read_buff, len);           //Writing the data in newly created file--> lunix.txt
